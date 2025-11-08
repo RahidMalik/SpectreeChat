335d7d541527530b40c1bed6c7bde63c2b3bb865
Rahidmalik <malikrahid011@gmail.com>
2025-11-08 21:49:35 +0500

    axios change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://spectreechatbackend-1.onrender.com",
+    baseURL: "https://spectreechatbackend-1.onrender.com/api",
     withCredentials: true,
 });
