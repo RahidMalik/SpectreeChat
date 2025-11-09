5d936a79d48fe5a5f2e674439d7ff5905f18ea3e
Rahidmalik <malikrahid011@gmail.com>
2025-11-09 20:34:00 +0500

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://spectreechatbackend-1.onrender.com/api",
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:5000/api"
+        : "https://spectreechatbackend-1.onrender.com/api",
     withCredentials: true,
 });
