9870d96e79bb954af7a0a941d1fb0ea31db4a31b
Rahidmalik <malikrahid011@gmail.com>
2025-11-10 21:17:03 +0500

    axios intanse

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const SOCKET_URL = import.meta.env.MODE === "development"
     ? "http://localhost:5000"
     : "https://spectreechatbackend-1.onrender.com";
 
-export const socket = io(SOCKET_URL, {
+export const axiosInstance = io(SOCKET_URL, {
     withCredentials: true,
     transports: ["websocket", "polling"], // ✅ Important!
     autoConnect: false, // Connect manually after auth
